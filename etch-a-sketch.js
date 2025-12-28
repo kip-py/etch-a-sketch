2c2b0403f53a64c2092bac1c16353214f6676003
kip-py <elena.thai@gmail.com>
2025-12-28 01:40:13 -0500

    add size changing

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -2,7 +2,8 @@ const container = document.querySelector("#container");
 container.setAttribute("style","display: flex; flex-direction: column; justify-content: center; align-items: center; border: solid; width:min-content;align-self:center")
 
 function createGrid(size)
-    {let pixels = Math.floor(480 / size)
+    {let pixels = 500 / size
+    container.replaceChildren();
     for (let j = 0; j < size; j++) {
         let row = document.createElement("div");
         row.classList.add("row");
@@ -11,7 +12,7 @@ function createGrid(size)
 
         for (let i = 0; i < size; i++) {
             let square = document.createElement("div");
-            square.setAttribute("style", "border-style: solid; background-color: black; height: 30px; width: 30px; opacity: 0.0");
+            square.setAttribute("style", "background-color: black; height: "+ pixels +"px; width: "+pixels+"px; opacity: 0.0");
             square.classList.add("square");
             square.setAttribute("id","square"+(i+j*6).toString());
             square.addEventListener("mouseenter", (e) => {
@@ -24,4 +25,14 @@ function createGrid(size)
         container.appendChild(row);
     }}
 
+button = document.querySelector("#button")
+button.addEventListener("click", (e) => {
+    let size = prompt("Enter grid size:")
+    if (size > 100 || size <= 0 || size % 1 != 0) {
+        alert("invalid number.")
+    }
+    else {
+        createGrid(size)
+    }
+})
 createGrid(16)
